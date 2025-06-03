cf78fb399f7fe7dd2b8f34f344fbe27be245a13b
M Aditya Bintang Pramudya <adityabintang197@gmail.com>
2025-06-03 11:25:28 +0700

    Tourguide UI

--- AdaTour.py
+++ AdaTour.py
@@ -1,6 +1,5 @@
-import os
+from . import lib
 import time
-
 from Controllers import *
 from Models import *
 from View import auth
@@ -17,19 +16,17 @@ except:
 
 users = {}
 
-def clear():
-    os.system('cls' if os.name == 'nt' else 'clear')
-
 def FirstStepMenu():
     while True:
-        clear()
-        print("===============================================")
-        print("||                 Menu Awal                 ||")
-        print("===============================================")
-        print("1. Login")
-        print("2. Registrasi (bila anda belum mempunyai akun)")
-        print("3. Keluar")
-        print("===============================================")
+        lib.clear_terminal()
+        text = "===============================================\n"
+        text+="||                 Menu Awal                 ||\n"
+        text+="===============================================\n"
+        text+="1. Login\n"
+        text+="2. Registtext+=bila anda belum mempunyai aku\n"
+        text+="3. Keluar\n"
+        text+="==============================================="
+        print(text)
         pilihan = input("Masukan inputan (1-3): ")
 
         if pilihan == '1':
@@ -37,47 +34,51 @@ def FirstStepMenu():
         elif pilihan == '2':
             MenuRegistrasi()
         elif pilihan == '3':
-            clear()
+            lib.clear_terminal()
             print("Terima kasih!")
             time.sleep(2)
             break
         else:
-            clear()
+            lib.clear_terminal()
             print("Pilihan tidak valid. Coba lagi.")
             time.sleep(2)
 
 def login():
-    clear()
-    print("==============================================")
-    print("||                Menu Login                ||")
-    print("==============================================")
-    username = input("Masukan username: ")
-    password = input("Masukan password: ")
+    while True:
+        lib.clear_terminal()
+        text += "=============================================="
+        text += "||                Menu Login                ||"
+        text += "=============================================="
+        print(text)
+        username = input("Masukan username: ")
+        password = input("Masukan password: ")
 
-    if username in users and users[username] == password:
-        clear()
-        print("==============================================")
-        print(f"||  Selamat datang di AdaTour, {username}!  ||")
-        print("==============================================")
-        time.sleep(2)
-        clear()
-        DashBoard()
-    else:
-        clear()
-        print("Login gagal. Username atau password salah.")
-        time.sleep(2)
+        if username in users and users[username] == password:
+            lib.clear_terminal()
+            text += "=============================================="
+            text += f"||  Selamat datang di AdaTour, {username}!  ||"
+            text += "=============================================="
+            print(text)
+            time.sleep(2)
+            lib.clear_terminal()
+            lib.DashBoard()
+        else:
+            lib.clear_terminal()
+            print("Login gagal. Username atau password salah.")
+            time.sleep(2)
 
 def MenuRegistrasi():
     while True:
-        clear()
-        print("===============================================")
-        print("||              Menu Registrasi              ||")
-        print("===============================================")
-        print("1. Register untuk pelanggan")
-        print("2. Register untuk Tourguide")
-        print("3. Log in (bila anda sudah mempunyai akun)")
-        print("4. Keluar")
-        print("===============================================")
+        lib.clear_terminal()
+        text += "==============================================="
+        text += "||              Menu Registrasi              ||"
+        text += "==============================================="
+        text += "1. Register untuk pelanggan"
+        text += "2. Register untuk Tourguide"
+        text += "3. Log in (bila anda sudah mempunyai akun)"
+        text += "4. Keluar"
+        text += "==============================================="
+        print(text)
         pilihan = input("Masukan inputan (1-4): ")
 
         if pilihan == '1':
@@ -87,82 +88,50 @@ def MenuRegistrasi():
         elif pilihan == '3':
             FirstStepMenu()
         elif pilihan == '4':
-            clear()
+            lib.clear_terminal()
             print("Terima kasih!")
             time.sleep(2)
             break
         else:
-            clear()
+            lib.clear_terminal()
             print("Pilihan tidak valid. Coba lagi.")
             time.sleep(2)
 
 def RegistrasiCustomer():
-    clear()
-    print("========================================")
-    print("||        Registrasi Pelanggan        ||")
-    print("========================================")
+    lib.clear_terminal()
+    text += "========================================"
+    text += "||        Registrasi Pelanggan        ||"
+    text += "========================================"
+    print(text)
     username = input("Buat username: ")
 
     if username in users:
-        clear()
+        lib.clear_terminal()
         print("Username sudah ada")
         time.sleep(2)
     else:
         password = input("Buat password: ")
         users[username] = password
 
-        clear()
+        lib.clear_terminal()
         print("Registrasi berhasil. Silakan login.")
         time.sleep(2)
 
 def RegistrasiTourGuide():
-    clear()
-    print("=========================================")
-    print("||         Registrasi TourGuide        ||")
-    print("=========================================")
+    lib.clear_terminal()
+    text += "========================================="
+    text += "||         Registrasi TourGuide        ||"
+    text += "========================================="
+    print(text)
     username = input("Buat username: ")
 
     if username in users:
-        clear()
+        lib.clear_terminal()
         print("Username sudah ada")
         time.sleep(2)
     else:
         password = input("Buat password: ")
         users[username] = password
-        clear()
+        lib.clear_terminal()
         print("Registrasi berhasil. Silakan login.")
         time.sleep(2)
-
-def DashBoard():
-    clear()
-    while True:
-        print("========================================")
-        print("||              DashBoard             ||")
-        print("========================================")
-        print("1. ")
-        print("2. ")
-        print("3. ")
-        print("4. Keluar")
-        print("========================================")
-        pilihan = input("Masukan inputan (1-4): ")
-
-        if pilihan == '1':
-            none()
-        elif pilihan == '2':
-            none()
-        elif pilihan == '3':
-            none()
-        elif pilihan == '4':
-            clear()
-            print("Terima kasih!")
-            time.sleep(2)
-            break
-        else:
-            clear()
-            print("Pilihan tidak valid. Coba lagi.")
-            time.sleep(2)
-
-def none():
-    print("isi dulu lee")
-
-FirstStepMenu()
